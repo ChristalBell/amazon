0686eab4b33d19ab95d76071052fbef1305a0d8c
Christal Bell <christalvictoriabell@gmail.com>
2024-06-27 11:56:23 -0700

    initial files, api

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,26 @@
+"use client";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    setProduct(() => {});
+  }, []);
+
+  fetch("https://fakestoreapi.com/products")
+    .then((response) => response.json())
+    .then((data) => setProduct(data))
+    .catch((error) => {
+      console.error(error);
+    });
+  console.log(product);
+
   return (
     <Box>
-      <Typography variant="h1">Home</Typography>
+      <Typography variant="h1">Hello World</Typography>
     </Box>
   );
 };
