b7fc6818a23a319ebc8d2be0ecfad15c4a53ef55
Christal Bell <christalvictoriabell@gmail.com>
2024-06-27 21:14:52 -0700

    styling header and second header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -6,8 +6,28 @@ import SearchBar from "./SearchBar";
 import HeaderLink from "./HeaderLink";
 import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
+  const linkList = [
+    "Medical Care",
+    "Groceries",
+    "Best Sellers",
+    "Amazon Basics",
+    "Prime",
+    "Music",
+    "New Releases",
+    "Customer Service",
+    "Today's Deals",
+    "Registry",
+    "Books",
+    "Pharmacy",
+    "Amazon Home",
+    "Gift Cards",
+    "Fashion",
+    "Luxury Stores",
+  ];
+
   return (
     <Box
       sx={{
@@ -44,6 +64,54 @@ const Header = () => {
             </Typography>
           </HeaderLink>
         </Box>
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: COLORS.mediumBlue,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <HeaderLink
+          href="/all"
+          style={{ display: "flex", alignItems: "center" }}
+        >
+          {" "}
+          <MenuIcon
+            sx={{ marginRight: "0.25rem", "&:hover": { color: COLORS.orange } }}
+          />
+          <Typography
+            variant="body2"
+            sx={{ fontWeight: 700, "&:hover": { color: COLORS.orange } }}
+          >
+            All
+          </Typography>
+        </HeaderLink>
+        {linkList.map((link) => {
+          return (
+            <HeaderLink
+              href={`/${link}`}
+              key={link}
+              style={{ marginRight: "0.5rem" }}
+            >
+              <Typography
+                variant="body2"
+                sx={{ "&:hover": { color: COLORS.orange } }}
+              >
+                {link}
+              </Typography>
+            </HeaderLink>
+          );
+        })}
+        <HeaderLink href="shop-black-owned">
+          <Typography
+            variant="body2"
+            sx={{ fontWeight: 700, "&:hover": { color: COLORS.orange } }}
+          >
+            Shop Black-owned businesses
+          </Typography>
+        </HeaderLink>
       </Box>
     </Box>
   );
