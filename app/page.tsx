5c5335e9d1ab2db476a93466674c40327acde00d
Christal Bell <christalvictoriabell@gmail.com>
2024-06-27 19:53:36 -0700

    Clean use effect

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,18 @@ const Home = () => {
   const [product, setProduct] = useState({});
 
   useEffect(() => {
-    setProduct(() => {});
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
-
-  fetch("https://fakestoreapi.com/products")
-    .then((response) => response.json())
-    .then((data) => setProduct(data))
-    .catch((error) => {
-      console.error(error);
-    });
   console.log(product);
 
   return (
     <Box>
-      <Typography variant="h1">Hello World</Typography>
+      <Typography variant="h1">Home</Typography>
     </Box>
   );
 };
