1e915c4e54bd963fdcf26cacf8616d8ccf1e5706
Christal Bell <christalvictoriabell@gmail.com>
2024-06-28 21:35:45 -0700

    styling product card

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,11 +1,36 @@
+import { COLORS } from "@/styles/colors";
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Ratings from "./shared/Ratings";
 
 const ProductCard = ({ product }: { product: any }) => {
   return (
-    <Box>
-      {product.title}
-      {product.price}
+    <Box
+      sx={{
+        backgroundColor: COLORS.white,
+        width: 225,
+        height: 450,
+        margin: ".5rem",
+        padding: "1rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Image alt={product.title} src={product.image} width={225} height={257} />
+      <Link href={`/product/${product.id}`} style={{ textDecoration: "none" }}>
+        <Typography variant="h2" sx={{ fontWeight: 500, color: COLORS.black }}>
+          {product.title.substring(0, 15)}...
+        </Typography>
+        <Ratings rating={product.rating} />
+        <Typography variant="h2" sx={{ fontWeight: 700, color: COLORS.black }}>
+          ${product.price}
+        </Typography>
+      </Link>
     </Box>
   );
 };
---
+++ components/shared/Ratings.tsx
@@ -0,0 +1,30 @@
+import { COLORS } from "@/styles/colors";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import Image from "next/image";
+import React from "react";
+
+const Ratings = ({ rating }: { rating: any }) => {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", margin: "0.5rem 0" }}>
+      <Box sx={{ marginRight: "0.5rem" }}>
+        <Image alt="star" src="/star-icon.png" height={20} width={20} />
+        <Image alt="star" src="/star-icon.png" height={20} width={20} />
+        <Image alt="star" src="/star-icon.png" height={20} width={20} />
+        <Image alt="star" src="/star-icon.png" height={20} width={20} />
+      </Box>
+      <Typography
+        variant="h2"
+        sx={{
+          fontWeight: 500,
+          color: COLORS.teal,
+          "&:hover": { color: COLORS.orange },
+        }}
+      >
+        {rating.count}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Ratings;
