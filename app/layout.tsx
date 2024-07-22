e8eab69384c768b933539d93f60317265ef59836
Christal Bell <christalvictoriabell@gmail.com>
2024-07-21 22:01:39 -0700

    added store setup?

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "@mui/material";
 import theme from "@/styles/theme";
 import { COLORS } from "@/styles/colors";
 import Header from "@/components/shared/Header";
+import { store } from ".//../store/index";
+import { Provider as ReduxProvider } from "react-redux";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -25,9 +27,11 @@ export default function RootLayout({
         style={{ backgroundColor: COLORS.lightGrey, margin: 0 }}
       >
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>
-            <Header /> {children}
-          </ThemeProvider>
+          <ReduxProvider store={store}>
+            <ThemeProvider theme={theme}>
+              <Header /> {children}
+            </ThemeProvider>
+          </ReduxProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
