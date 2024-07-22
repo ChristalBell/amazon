0c4192454b13c48b3d70097219172976e077ba69
Christal Bell <christalvictoriabell@gmail.com>
2024-07-22 00:27:05 -0700

    api and filter working

--- app/search/[query]/page.tsx
+++ app/search/[query]/page.tsx
@@ -2,27 +2,64 @@
 import SearchResults from "@/components/SearchResults";
 import { Home } from "@mui/icons-material";
 import { useParams } from "next/navigation";
-import { title } from "process";
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const QueryPage = () => {
   const { query } = useParams();
 
-  // const filterProducts = () => {
-  //   return products.filter((product) => {
-  //     return product.title.toLowerCase().includes(query);
-  //   });
-  // };
+  const [products, setProducts] = useState([
+    {
+      category: "",
+      description: "",
+      id: 0,
+      image: "",
+      price: 0,
+      rating: { rate: 0, count: 0 },
+      title: "",
+    },
+  ]);
 
-  // const filteredProducts = filterProducts();
-  // console.log("the bottom is found ");
-  // console.log(filteredProducts);
+  const [isLoading, setIsLoading] = useState(true);
+
+  console.log(products);
+
+  // test filter
+
+  const filterProducts = () => {
+    return products.filter((product) => {
+      return product.title.toLowerCase().includes(query.toString());
+    });
+  };
+  // console.log(searchItems());
+
+  const filteredProducts = filterProducts();
+  console.log("the bottom is found ");
+  console.log(filteredProducts);
+
+  // end of test filter
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  // console.log(products);
+
+  if (isLoading) {
+    return <div> Loading...</div>;
+  }
 
   return (
     <div>
       <p>query page</p>
-      {/* {<SearchResults products={filteredProducts} />} */}
+      {<SearchResults products={filteredProducts} />}
     </div>
   );
 };
