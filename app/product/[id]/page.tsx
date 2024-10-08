86e2aac510fd6a21b137f14d3ba4013793f0b2a6
Christal Bell <christalvictoriabell@gmail.com>
2024-10-08 12:54:28 -0700

    single product target work

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -2,15 +2,21 @@
 import Box from "@mui/material/Box";
 import { Container } from "@mui/material";
 import { useParams } from "next/navigation";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/index";
-
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/index";
 import ProductDetails from "@/components/ProductDetails";
-import { inherits } from "util";
-
+import { fetchData } from "../../../store/productsSlice";
+import { useEffect } from "react";
 const ProductPage = () => {
   const products = useSelector((state: RootState) => state.api.data);
+  const dispatch = useDispatch<AppDispatch>();
+
   const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(fetchData(id));
+  }, [dispatch]);
+
   console.log(id);
   console.log(products);
 
