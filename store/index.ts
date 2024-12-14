14dea50c881d5f75cb4297394c53b5c0366e2913
Christal Bell <christalvictoriabell@gmail.com>
2024-12-13 17:34:28 -0800

    app function atm

--- store/index.ts
+++ store/index.ts
@@ -6,15 +6,3 @@ export const store = configureStore({
     cartItems: cartItemReducer,
   },
 });
-
-// export const store = configureStore({
-//   reducer: {
-//     [apiSlice.reducerPath]: apiSlice.reducer,
-//     [cartItemSlice.reducerPath]: cartItemSlice.reducer,
-
-//     middleware: () => new Tuple(apiSlice),
-//   },
-// });
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
